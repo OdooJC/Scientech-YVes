3ca3dd364179106e459b66c5135e6b29b48b5853
Jean Paul Astudillo C <jpastucon@gmail.com>
2023-10-30 03:53:48 -0300

    Custom ResPartner - Zona Socios v1.0.13

--- jpastucon/Custom/res_partner_zona_clasificacion/models/ResPartner.py
+++ jpastucon/Custom/res_partner_zona_clasificacion/models/ResPartner.py
@@ -5,48 +5,101 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     zona_clasificacion = fields.Selection(
-        selection=[
-            ("Zona Norte", "Zona Norte"),
-            ("Zona Centro", "Zona Centro"),
-            ("Zona Sur 1", "Zona Sur 1"),
-            ("Zona Sur 2", "Zona Sur 2"),
-            ("Zona Metropolitana", "Zona Metropolitana"),
+        [
+            ("zona_norte", "Zona Norte"),
+            ("zona_centro", "Zona Centro"),
+            ("zona_sur_1", "Zona Sur 1"),
+            ("zona_sur_2", "Zona Sur 2"),
+            ("zona_metropolitana", "Zona Metropolitana"),
         ],
         string="Zona de Clasificación",
-        help="Seleccione la clasificación correspondiente.",
+        compute="_compute_zona_clasificacion",
         store=True,
-        index=True,
     )
 
-    @api.onchange("state_id")
-    def _onchange_state_id(self):
-        if self.state_id:
-            state_name = self.state_id.name
-
-            if state_name in (
-                "Arica y Parinacota",
-                "Tarapacá",
-                "Antofagasta",
-                "Atacama",
-                "Coquimbo",
-            ):
-                self.zona_clasificacion = "Zona Norte"
-            elif state_name in (
-                "Valparaíso",
-                "del Libertador Gral. Bernardo O'higgins",
-                "del Maule",
-            ):
-                self.zona_clasificacion = "Zona Centro"
-            elif state_name in ("del Ñuble", "del BíoBio", "de la Araucania"):
-                self.zona_clasificacion = "Zona Sur 1"
-            elif state_name in (
-                "Los Ríos",
-                "de los Lagos",
-                "Aysén del Gral. Carlos Ibáñez del Campo",
-                "Magallanes",
-            ):
-                self.zona_clasificacion = "Zona Sur 2"
-            elif state_name == "Metropolitana":
-                self.zona_clasificacion = "Zona Metropolitana"
+    @api.depends("state_id")
+    def _compute_zona_clasificacion(self):
+        for partner in self:
+            state = partner.state_id
+            if state:
+                state_code = state.code
+                if state_code in [
+                    "CL01000",
+                    "CL01100",
+                    "CL01400",
+                    "CL02000",
+                    "CL02100",
+                    "CL02200",
+                    "CL02300",
+                    "CL03000",
+                    "CL03100",
+                    "CL03200",
+                    "CL03300",
+                    "CL04000",
+                    "CL04100",
+                    "CL04200",
+                    "CL04300",
+                ]:
+                    partner.zona_clasificacion = "zona_norte"
+                elif state_code in [
+                    "CL05000",
+                    "CL05100",
+                    "CL05200",
+                    "CL05300",
+                    "CL05400",
+                    "CL05500",
+                    "CL05600",
+                    "CL05700",
+                    "CL06000",
+                    "CL06100",
+                    "CL06200",
+                    "CL06300",
+                    "CL07000",
+                    "CL07100",
+                    "CL07200",
+                    "CL07300",
+                    "CL07400",
+                ]:
+                    partner.zona_clasificacion = "zona_centro"
+                elif state_code in [
+                    "CL08000",
+                    "CL08100",
+                    "CL08200",
+                    "CL08300",
+                    "CL09000",
+                    "CL09100",
+                    "CL09200",
+                    "CL10000",
+                    "CL10100",
+                    "CL10200",
+                    "CL10300",
+                    "CL10400",
+                ]:
+                    partner.zona_clasificacion = "zona_sur_1"
+                elif state_code in [
+                    "CL11000",
+                    "CL11100",
+                    "CL11200",
+                    "CL11300",
+                    "CL11400",
+                    "CL12000",
+                    "CL12100",
+                    "CL12200",
+                    "CL12300",
+                    "CL12400",
+                ]:
+                    partner.zona_clasificacion = "zona_sur_2"
+                elif state_code in [
+                    "CL13000",
+                    "CL13100",
+                    "CL13200",
+                    "CL13300",
+                    "CL13400",
+                    "CL13500",
+                    "CL13600",
+                ]:
+                    partner.zona_clasificacion = "zona_metropolitana"
+                else:
+                    partner.zona_clasificacion = False
             else:
-                self.zona_clasificacion = False
+                partner.zona_clasificacion = False
