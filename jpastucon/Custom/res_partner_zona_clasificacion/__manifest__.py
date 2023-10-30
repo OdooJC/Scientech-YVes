b77ebdd2b8aad2be9a775dc96d87f2dff8ccb49b
Jean Paul Astudillo C <jpastucon@gmail.com>
2023-10-30 02:41:02 -0300

    Custom ResPartner - Zona Socios v1.0.0

---
+++ jpastucon/Custom/res_partner_zona_clasificacion/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Clasificación Zona Socios",
+    "version": "1.0",
+    "summary": "Verifica/modifica las opciones de selección en el campo x_studio_zona_clasificacion",
+    "description": "Este módulo permite verificar y modificar las opciones de selección del campo x_studio_zona_clasificacion en el modelo de contactos.",
+    "category": "Customization",
+    "author": "jpastucon",
+    "depends": ["base", "contacts"],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
---
+++ jpastucon/Custom/res_partner_zona_clasificacion/models/ResPartner.py
@@ -0,0 +1,52 @@
+from odoo import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    x_studio_zona_clasificacion = fields.Selection(
+        selection=[
+            ("Zona Norte", "Zona Norte"),
+            ("Zona Centro", "Zona Centro"),
+            ("Zona Sur 1", "Zona Sur 1"),
+            ("Zona Sur 2", "Zona Sur 2"),
+            ("Zona Metropolitana", "Zona Metropolitana"),
+        ],
+        string="Zona de Clasificación",
+        help="Seleccione la clasificación correspondiente.",
+        store=True,
+        index=True,
+    )
+
+    @api.onchange("state_id")
+    def _onchange_state_id(self):
+        if self.state_id:
+            state_name = self.state_id.name
+
+            if state_name in (
+                "Arica y Parinacota",
+                "Tarapacá",
+                "Antofagasta",
+                "Atacama",
+                "Coquimbo",
+            ):
+                self.x_studio_zona_clasificacion = "Zona Norte"
+            elif state_name in (
+                "Valparaíso",
+                "del Libertador Gral. Bernardo O'higgins",
+                "del Maule",
+            ):
+                self.x_studio_zona_clasificacion = "Zona Centro"
+            elif state_name in ("del Ñuble", "del BíoBio", "de la Araucania"):
+                self.x_studio_zona_clasificacion = "Zona Sur 1"
+            elif state_name in (
+                "Los Ríos",
+                "de los Lagos",
+                "Aysén del Gral. Carlos Ibáñez del Campo",
+                "Magallanes",
+            ):
+                self.x_studio_zona_clasificacion = "Zona Sur 2"
+            elif state_name == "Metropolitana":
+                self.x_studio_zona_clasificacion = "Zona Metropolitana"
+            else:
+                self.x_studio_zona_clasificacion = False
